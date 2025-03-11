84d8665813fa51e04b76e138cb2d82d4c889f983
James McCorrie <james.mccorrie@lowrisc.org>
2025-03-11 12:04:17 +0000

    [nix] fix the flake nixpkgs and relock
    Signed-off-by: James McCorrie <james.mccorrie@lowrisc.org>

--- flake.nix
+++ flake.nix
@@ -2,6 +2,9 @@
   description = "DVSim development environment";
 
   inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+
     flake-utils.url = "github:numtide/flake-utils";
 
     pyproject-nix.url = "github:nix-community/pyproject.nix";
@@ -29,6 +32,7 @@
   outputs = {
     self,
     nixpkgs,
+    nixpkgs-unstable,
     flake-utils,
     uv2nix,
     pyproject-nix,
@@ -47,58 +51,59 @@
 
       pyprojectOverrides = _final: _prev: {
         dvsim = _prev.dvsim.overrideAttrs (old: {
-          passthru = old.passthru // {
-            # Put all tests in the passthru.tests attribute set.
-            # Nixpkgs also uses the passthru.tests mechanism for ofborg test discovery.
-            #
-            # For usage with Flakes we will refer to the passthru.tests attributes to construct the flake checks attribute set.
-            tests =
-              let
+          passthru =
+            old.passthru
+            // {
+              # Put all tests in the passthru.tests attribute set.
+              # Nixpkgs also uses the passthru.tests mechanism for ofborg test discovery.
+              #
+              # For usage with Flakes we will refer to the passthru.tests attributes to construct the flake checks attribute set.
+              tests = let
                 # Construct a virtual environment with only the test dependency-group enabled for testing.
                 virtualenv = _final.mkVirtualEnv "dvsim-pytest-env" {
-                  dvsim = [ "test" ];
+                  dvsim = ["test"];
                 };
-
               in
-              (old.tests or { })
-              // {
-                pytest = pkgs.stdenv.mkDerivation {
-                  name = "${_final.dvsim.name}-pytest";
-                  inherit (_final.dvsim) src;
-                  nativeBuildInputs = [
-                    virtualenv
-                  ];
-                  dontConfigure = true;
+                (old.tests or {})
+                // {
+                  pytest = pkgs.stdenv.mkDerivation {
+                    name = "${_final.dvsim.name}-pytest";
+                    inherit (_final.dvsim) src;
+                    nativeBuildInputs = [
+                      virtualenv
+                    ];
+                    dontConfigure = true;
 
-                  # Because this package is running tests, and not actually building the main package
-                  # the build phase is running the tests.
-                  #
-                  # In this particular example we also output a HTML coverage report, which is used as the build output.
-                  buildPhase = ''
-                    runHook preBuild
-                    pytest --cov-report html
-                    runHook postBuild
-                  '';
+                    # Because this package is running tests, and not actually building the main package
+                    # the build phase is running the tests.
+                    #
+                    # In this particular example we also output a HTML coverage report, which is used as the build output.
+                    buildPhase = ''
+                      runHook preBuild
+                      pytest --cov-report html
+                      runHook postBuild
+                    '';
 
-                  # Install the HTML coverage report into the build output.
-                  #
-                  # If you wanted to install multiple test output formats such as TAP outputs
-                  # you could make this derivation a multiple-output derivation.
-                  #
-                  # See https://nixos.org/manual/nixpkgs/stable/#chap-multiple-output for more information on multiple outputs.
-                  installPhase = ''
-                    runHook preInstall
-                    mv htmlcov $out
-                    runHook postInstall
-                  '';
+                    # Install the HTML coverage report into the build output.
+                    #
+                    # If you wanted to install multiple test output formats such as TAP outputs
+                    # you could make this derivation a multiple-output derivation.
+                    #
+                    # See https://nixos.org/manual/nixpkgs/stable/#chap-multiple-output for more information on multiple outputs.
+                    installPhase = ''
+                      runHook preInstall
+                      mv htmlcov $out
+                      runHook postInstall
+                    '';
+                  };
                 };
-              };
-          };
+            };
         });
       };
 
       pkgs = nixpkgs.legacyPackages.${system};
-      python = pkgs.python312;
+      pkgs-unstable = nixpkgs-unstable.legacyPackages.${system};
+      python = pkgs.python313;
       pythonSet =
         # Use base package set from pyproject.nix builders
         (pkgs.callPackage pyproject-nix.build.packages {
@@ -128,7 +133,7 @@
           packages = [
             python
             pkgs.uv
-            pkgs.ruff
+            pkgs-unstable.ruff
             pkgs.pyright
           ];
           env =
