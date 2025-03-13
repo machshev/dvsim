c6852171daa6816973daf56ac8ff20f5ec0598d0
James McCorrie <james.mccorrie@lowrisc.org>
2025-03-13 18:39:31 +0000

    [utils] split out remaining utils into modules
    Signed-off-by: James McCorrie <james.mccorrie@lowrisc.org>

---
+++ src/dvsim/utils/hjson.py
@@ -0,0 +1,32 @@
+# Copyright lowRISC contributors (OpenTitan project).
+# Licensed under the Apache License, Version 2.0, see LICENSE for details.
+# SPDX-License-Identifier: Apache-2.0
+"""Utility functions for hjson."""
+
+import logging as log
+import sys
+from collections.abc import Mapping
+from pathlib import Path
+
+from hjson import loads
+
+
+def decode_hjson(s: str) -> Mapping[str, object]:
+    """Decode data as HJSON."""
+    return loads(s=s, use_decimal=True)
+
+
+def parse_hjson(hjson_file: Path | str) -> Mapping:
+    """Parse hjson and return a dict."""
+    log.debug("Parsing %s", hjson_file)
+
+    try:
+        return decode_hjson(s=Path(hjson_file).read_text())
+
+    except Exception as e:
+        log.fatal(
+            'Failed to parse "%s" possibly due to bad path or syntax error.\n%s',
+            hjson_file,
+            e,
+        )
+        sys.exit(1)
---
+++ src/dvsim/utils/status_printer.py
@@ -0,0 +1,224 @@
+# Copyright lowRISC contributors (OpenTitan project).
+# Licensed under the Apache License, Version 2.0, see LICENSE for details.
+# SPDX-License-Identifier: Apache-2.0
+"""Status printing utilities."""
+
+import logging as log
+import sys
+
+import enlighten
+
+
+class StatusPrinter:
+    """Dummy Status Printer class for interactive mode.
+
+    When interactive mode is set, dvsim does not print the status. By
+    instantiating this dummy class (printing nothing), outer interface stays
+    same.
+    """
+
+    def __init__(self) -> None:
+        """Initialise."""
+
+    def print_header(self, msg: str) -> None:
+        """Print header."""
+
+    def init_target(self, target: str, msg: str) -> None:
+        """Initialise target."""
+
+    def update_target(
+        self,
+        target: str,
+        hms: str,
+        msg: str,
+        perc,
+        running,
+    ) -> None:
+        """Update target.
+
+        Args:
+            hms: Elapsed time in hh:mm:ss.
+            target: The tool flow step.
+            msg: The completion status message (set externally).
+            perc: Percentage of completion.
+            running: What jobs are currently still running.
+
+        """
+
+    def exit(self) -> None:
+        """Exit."""
+
+
+class TtyStatusPrinter(StatusPrinter):
+    """Abstraction for printing the current target status onto the console.
+
+    Targets are ASIC tool flow steps such as build, run, cov etc. These steps
+    are sequenced by the Scheduler. There may be multiple jobs running in
+    parallel in each target. This class provides a mechanism to periodically
+    print the completion status of each target onto the terminal. Messages
+    printed by this class are rather static in nature - all the necessary
+    computations of how the jobs are progressing need to be handled externally.
+
+    The following are the 'fields' accepted by this class:
+      hms:      Elapsed time in hh:mm:ss.
+      target:   The tool flow step.
+      msg:      The completion status message (set externally).
+      perc:     Percentage of completion.
+      running:  What jobs are currently still running.
+    """
+
+    # Print elapsed time in bold.
+    hms_fmt = "\x1b[1m{hms:9s}\x1b[0m"
+    header_fmt = hms_fmt + " [{target:^13s}]: [{msg}]"
+    status_fmt = header_fmt + " {perc:3.0f}%  {running}"
+
+    def __init__(self) -> None:
+        """Once a target is complete, we no longer need to update it - we can
+        just skip it. Maintaining this here provides a way to print the status
+        one last time when it reaches 100%. It is much easier to do that here
+        than in the Scheduler class.
+        """
+        super().__init__()
+        self.target_done = {}
+
+    def print_header(self, msg) -> None:
+        """Initialize / print the header bar.
+
+        The header bar contains an introductory message such as the legend of
+        what Q, D, ... mean.
+        """
+        log.info(self.header_fmt.format(hms="", target="legend", msg=msg))
+
+    def init_target(self, target, msg) -> None:
+        """Initialize the status bar for each target."""
+        self.target_done[target] = False
+
+    def _trunc_running(self, running: str) -> str:
+        """Truncates the list of running items to 30 character string."""
+        return running[:28] + (running[28:] and "..")
+
+    def update_target(self, target, hms, msg, perc, running) -> None:
+        """Periodically update the status bar for each target."""
+        if self.target_done[target]:
+            return
+
+        log.info(
+            self.status_fmt.format(
+                hms=hms,
+                target=target,
+                msg=msg,
+                perc=perc,
+                running=self._trunc_running(running),
+            ),
+        )
+        if perc == 100:
+            self.target_done[target] = True
+
+    def exit(self) -> None:
+        """Do cleanup activities before exiting."""
+
+
+class EnlightenStatusPrinter(TtyStatusPrinter):
+    """Abstraction for printing status using Enlighten.
+
+    Enlighten is a third party progress bar tool. Documentation:
+    https://python-enlighten.readthedocs.io/en/stable/
+
+    Though it offers very fancy progress bar visualization, we stick to a
+    simple status bar 'pinned' to the bottom of the screen for each target
+    that displays statically, a pre-prepared message. We avoid the progress bar
+    visualization since it requires enlighten to perform some computations the
+    Scheduler already does. It also helps keep the overhead to a minimum.
+
+    Enlighten does not work if the output of dvsim is redirected to a file, for
+    example - it needs to be attached to a TTY enabled stream.
+    """
+
+    def __init__(self) -> None:
+        super().__init__()
+
+        # Initialize the status_bars for header and the targets .
+        self.manager = enlighten.get_manager()
+        self.status_header = None
+        self.status_target = {}
+
+    def print_header(self, msg) -> None:
+        self.status_header = self.manager.status_bar(
+            status_format=self.header_fmt,
+            hms="",
+            target="legend",
+            msg="Q: queued, D: dispatched, P: passed, F: failed, K: killed, T: total",
+        )
+
+    def init_target(self, target, msg) -> None:
+        super().init_target(target, msg)
+        self.status_target[target] = self.manager.status_bar(
+            status_format=self.status_fmt,
+            hms="",
+            target=target,
+            msg=msg,
+            perc=0.0,
+            running="",
+        )
+
+    def update_target(self, target, hms, msg, perc, running) -> None:
+        if self.target_done[target]:
+            return
+
+        self.status_target[target].update(
+            hms=hms,
+            msg=msg,
+            perc=perc,
+            running=self._trunc_running(running),
+        )
+        if perc == 100:
+            self.target_done[target] = True
+
+    def exit(self) -> None:
+        self.status_header.close()
+        for target in self.status_target:
+            self.status_target[target].close()
+
+
+def get_status_printer(*, interactive: bool) -> StatusPrinter:
+    """Factory method that returns a status printer instance.
+
+    If stdout is a TTY, then return an instance of EnlightenStatusPrinter, else
+    return an instance of StatusPrinter.
+    """
+    if interactive:
+        return StatusPrinter()
+
+    if sys.stdout.isatty():
+        return EnlightenStatusPrinter()
+
+    return TtyStatusPrinter()
+
+
+def print_msg_list(msg_list_title, msg_list, max_msg_count=-1):
+    """This function prints a list of messages to Markdown.
+
+    The argument msg_list_title contains a string for the list title, whereas
+    the msg_list argument contains the actual list of message strings.
+    max_msg_count limits the number of messages to be printed (set to negative
+    number to print all messages).
+
+    Example:
+    print_msg_list("### Tool Warnings", ["Message A", "Message B"], 10)
+
+    """
+    md_results = ""
+    if msg_list:
+        md_results += msg_list_title + "\n"
+        md_results += "```\n"
+        for k, msg in enumerate(msg_list):
+            if k <= max_msg_count or max_msg_count < 0:
+                md_results += msg + "\n\n"
+            else:
+                md_results += "Note: %d more messages have been suppressed " % (
+                    len(msg_list) - max_msg_count
+                )
+                md_results += "(max_msg_count = %d) \n\n" % (max_msg_count)
+                break
+        md_results += "```\n"
+    return md_results
