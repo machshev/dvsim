6027d679ac1809257d768c97e22aca4622cb0f70
James McCorrie <james.mccorrie@lowrisc.org>
2025-03-26 16:39:17 +0000

    [templates] add basic template rendering infrastructure
    Signed-off-by: James McCorrie <james.mccorrie@lowrisc.org>

---
+++ src/dvsim/templates/render.py
@@ -0,0 +1,43 @@
+# SPDX-FileCopyrightText: lowRISC contributors (OpenTitan project).
+# SPDX-License-Identifier: Apache-2.0
+"""Render template."""
+
+from collections.abc import Mapping
+from pathlib import Path
+
+from logzero import logger
+from mako import exceptions
+from mako.template import Template
+
+
+def render_template(path: Path, data: Mapping[str, object] | None = None) -> str:
+    """Render a template from the relative path.
+
+    Args:
+        path: relative path to the template
+        data: mapping of key/value pairs to send to the template renderer
+
+    Returns:
+        string containing the rendered template
+
+    """
+    template_base_path = Path(__file__).parent
+    template_path = template_base_path / path
+
+    if not template_path.exists():
+        logger.error("Template file not found: %s", template_path)
+        raise FileNotFoundError
+
+    try:
+        output = Template(template_path.read_text()).render(**data or {})  # noqa: S702
+
+    except NameError:
+        # The NameError exception doesn't contain a useful error message. this
+        # has to ge requested seporatly from Mako for some reason?
+        logger.error(exceptions.text_error_template().render())
+        raise
+
+    if isinstance(output, bytes):
+        output = output.decode(encoding="utf8")
+
+    return output
