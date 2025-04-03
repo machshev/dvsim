9b4f89fcefa43e291998d26474678e066e5e4431
James McCorrie <james.mccorrie@lowrisc.org>
2025-04-03 14:13:34 +0100

    refactor: rename dvsim.Test/Testplan -> dvsim.utils.test/testplan
    Signed-off-by: James McCorrie <james.mccorrie@lowrisc.org>

--- src/dvsim/regression.py
+++ src/dvsim/regression.py
@@ -6,7 +6,7 @@ import logging as log
 import sys
 
 from dvsim.modes import Mode, find_mode, find_mode_list
-from dvsim.Test import Test
+from dvsim.test import Test
 from dvsim.utils import VERBOSE
 
 
