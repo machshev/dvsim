14c4917c9aa2a44aaaf1071e1ca5b19cf895bb89
James McCorrie <james.mccorrie@lowrisc.org>
2025-04-03 14:11:05 +0100

    refactor: rename dvsim.StatusPrinter -> dvsim.utils.status_printer
    Signed-off-by: James McCorrie <james.mccorrie@lowrisc.org>

--- src/dvsim/scheduler.py
+++ src/dvsim/scheduler.py
@@ -8,9 +8,9 @@ import threading
 from signal import SIGINT, SIGTERM, signal
 
 from dvsim.launcher.base import LauncherBusy, LauncherError
-from dvsim.StatusPrinter import get_status_printer
 from dvsim.Timer import Timer
 from dvsim.utils import VERBOSE
+from dvsim.utils.status_printer import get_status_printer
 
 
 # Sum of lengths of all lists in the given dict.
