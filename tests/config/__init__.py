0e0862f66c3b177649840b861e3535a4a4feb533
James McCorrie <james.mccorrie@lowrisc.org>
2025-09-30 15:28:46 +0100

    refactor: [config] improved config loading with testing
    Signed-off-by: James McCorrie <james.mccorrie@lowrisc.org>

--- tests/config/__init__.py
+++ tests/config/__init__.py
@@ -1,13 +1,5 @@
-<!--
 # Copyright lowRISC contributors (OpenTitan project).
 # Licensed under the Apache License, Version 2.0, see LICENSE for details.
 # SPDX-License-Identifier: Apache-2.0
 
--->
-# FOO DV document
-
-<!-- TODO: Remove "draft: true" from header before submitting -->
-
-# testplan
-
-{{< incGenFromIpDesc "foo_testplan.hjson" "testplan" >}}
+"""Test configuration infrastructure."""
