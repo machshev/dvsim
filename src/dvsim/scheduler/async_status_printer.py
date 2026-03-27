40610864bd9c1597639df496f83f33b76ea88c81
Alex Jones <alex.jones@lowrisc.org>
2026-03-27 12:27:14 +0000

    feat: port `EnlightenStatusPrinter` to the async interface
    This commit ports the original `EnlightenStatusPrinter` to the new async
    interface, extending the `StatusPrinter` abstract base class.
    
    The logic is mostly the same, with a few important caveats to note:
    - Since the new interface allows float (and hence sub-second) print
      intervals, a warning is introduced for intervals less than Enlighten's
      internal minimum delta value which will cause updates to be coalesced
      and potentially lost at points if not refreshed. We could also lower
      the `min_delta` to match the print interval, but experimentation shows
      that this introduces performance concerns and is best left as is.
    - Because of the above, logic is added to refresh (flush) the status bar
      when a target is done, to ensure it locks the final time correctly.
    - An occasional bug was encountered on using Ctrl-C to gracefully exit
      where Enlighten's `StatusBar.update` would hang indefinitely. This
      occurred during the terminal protocol used by the underlying Blessed
      library, which queried the terminal for its size and expected a
      response. Under heavy loads, particularly when a large number of
      processes are killed due to an exit signal, the terminal response
      might not be received, causing Blessed to hang on a `getch` call.
      To prevent this, the `pause` interface was introduced to the base
      `StatusPrinter` which is used for that purpose here.
    
    Signed-off-by: Alex Jones <alex.jones@lowrisc.org>

--- src/dvsim/scheduler/async_status_printer.py
+++ src/dvsim/scheduler/async_status_printer.py
@@ -5,10 +5,17 @@
 """Job status printing during a scheduled run."""
 
 import asyncio
+import os
+import shutil
+import sys
+import termios
 import time
 from abc import ABC, abstractmethod
 from collections import defaultdict
 from collections.abc import Sequence
+from typing import ClassVar
+
+import enlighten
 
 from dvsim.job.data import JobSpec
 from dvsim.job.status import JobStatus
@@ -223,3 +230,171 @@ class TtyStatusPrinter(StatusPrinter):
                 running=self._trunc_running(", ".join(self._running[target])),
             ),
         )
+
+
+class EnlightenStatusPrinter(TtyStatusPrinter):
+    """Prints the current scheduler target status to the terminal using Enlighten.
+
+    Enlighten is a third party progress bar tool. Documentation:
+    https://python-enlighten.readthedocs.io/en/stable/
+
+    Enlighten does not work if the output of DVSim is redirected to a file, for
+    example - it needs to be attached to a TTY enabled stream.
+    """
+
+    # Enlighten uses a min_delta of 0.1 by default, only updating every 0.1 seconds.
+    DEFAULT_MIN_DELTA = 0.1
+
+    status_fmt_no_running = TtyStatusPrinter.status_fmt.removesuffix("{running}")
+    status_fmt = "{status_msg}{running}"
+
+    def __init__(self, jobs: Sequence[JobSpec]) -> None:
+        """Initialise the EnlightenStatusPrinter."""
+        super().__init__(jobs)
+        if self._interval < self.DEFAULT_MIN_DELTA:
+            # TODO: maybe "debounce" the updates with a delayed async refresh task?
+            log.warning(
+                "Configured print interval %g will not accurately reflect for %s,"
+                " which uses status bars with a configured min_delta of %g by default.",
+                self._interval,
+                self.__class__.__name__,
+                self.DEFAULT_MIN_DELTA,
+            )
+
+        # Initialize the enlighten manager and needed state
+        self._manager = enlighten.get_manager()
+        self._status_header: enlighten.StatusBar | None = None
+        self._status_bars: dict[str, enlighten.StatusBar] = {}
+        self._stopped = False
+
+    def _print_header(self) -> None:
+        """Initialize / print the header, displaying the legend of job status meanings."""
+        self._status_header = self._manager.status_bar(
+            status_format=self.header_fmt,
+            hms="",
+            target="legend",
+            msg=self._get_header(),
+        )
+
+    def _init_target(self, target: str, msg: str) -> None:
+        """Initialize the status bar for a target."""
+        super()._init_target(target, msg)
+        hms = time.strftime(TS_HMS_FORMAT, time.gmtime(0))
+        msg = self.status_fmt_no_running.format(hms=hms, target=target, msg=msg, percent=0.0)
+        self._status_bars[target] = self._manager.status_bar(
+            status_format=self.status_fmt,
+            status_msg=msg,
+            running="",
+        )
+
+    def _trunc_running_to_terminal(self, running: str, offset: int) -> str:
+        """Truncate the list of running items to match the max terminal width."""
+        cols = shutil.get_terminal_size(fallback=(80, 24)).columns
+        width = max(30, cols - offset - 1)
+        return self._trunc_running(running, width)
+
+    def _update_target(self, current_time: float, target: str) -> None:
+        """Update the status bar for a given target."""
+        if self._target_done[target]:
+            return
+
+        status_counts = self._target_counts[target]
+        done_count = sum(status_counts[status] for status in JobStatus if status.is_terminal)
+        percent = (done_count / self._totals[target] * 100) if self._totals[target] else 100
+        elapsed_time = time.gmtime(current_time - self._start_time)
+
+        status_msg = self.status_fmt_no_running.format(
+            hms=time.strftime(TS_HMS_FORMAT, elapsed_time),
+            target=target,
+            msg=self._get_target_row(target),
+            percent=percent,
+        )
+        offset = len(status_msg)
+        running = self._trunc_running_to_terminal(", ".join(self._running[target]), offset)
+
+        self._status_bars[target].update(status_msg=status_msg, running=running)
+
+        if self.target_is_done(target):
+            self._target_done[target] = True
+            self._status_bars[target].refresh()
+
+    def stop(self) -> None:
+        """Stop the status header/target printing (but keep the printer context)."""
+        super().stop()
+        if self._status_header is not None:
+            self._status_header.close()
+        for status_bar in self._status_bars.values():
+            status_bar.close()
+        self._stopped = True
+
+    def exit(self) -> None:
+        """Do cleanup activities before exiting (closing the manager context)."""
+        super().exit()
+        if not self._stopped:
+            self.stop()
+        self._manager.stop()
+
+        # Sometimes, exiting via a signal (e.g. Ctrl-C) can cause Enlighten to leave the
+        # terminal in some non-raw mode. Just in case, restore regular operation.
+        self._restore_terminal()
+
+    def _restore_terminal(self) -> None:
+        """Restore regular terminal operation after using Enlighten."""
+        # Try open /dev/tty, otherwise fallback to sys.stdin
+        try:
+            fd = os.open("/dev/tty", os.O_RDWR)
+            close_fd = True
+        except (OSError, termios.error):
+            fd = sys.stdin.fileno()
+            close_fd = False
+
+        # By default, the terminal should echo input (ECHO) and run in canonical mode (ICANON).
+        # We make this change after all buffered output is transmitted (TCSADRAIN).
+        try:
+            attrs = termios.tcgetattr(fd)
+            attrs[3] |= termios.ECHO | termios.ICANON
+            termios.tcsetattr(fd, termios.TCSADRAIN, attrs)
+        except termios.error:
+            log.debug("Unable to restore terminal attributes safely")
+
+        if close_fd:
+            os.close(fd)
+
+
+class StatusPrinterSingleton:
+    """Singleton for the status printer to uniquely refer to 1 instance at a time."""
+
+    _instance: ClassVar[StatusPrinter | None] = None
+
+    @classmethod
+    def set(cls, instance: StatusPrinter | None) -> None:
+        """Set the stored status printer."""
+        cls._instance = instance
+
+    @classmethod
+    def get(cls) -> StatusPrinter | None:
+        """Get the stored status printer (if it exists)."""
+        return cls._instance
+
+
+def create_status_printer(jobs: Sequence[JobSpec]) -> StatusPrinter:
+    """Create the global status printer.
+
+    If stdout is a TTY, then return an instance of EnlightenStatusPrinter, else
+    return an instance of StatusPrinter.
+    """
+    status_printer = StatusPrinterSingleton.get()
+    if status_printer is not None:
+        return status_printer
+
+    status_printer = EnlightenStatusPrinter(jobs) if sys.stdout.isatty() else TtyStatusPrinter(jobs)
+    StatusPrinterSingleton.set(status_printer)
+    return status_printer
+
+
+def get_status_printer() -> StatusPrinter:
+    """Retrieve the configured global status printer."""
+    status_printer = StatusPrinterSingleton.get()
+    if status_printer is None:
+        raise RuntimeError("get_status_printer called without first creating the status printer")
+    return status_printer
